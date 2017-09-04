1f875d1ca996daeb1b8cd19e458c4eb4f46754e9
airosa-id <registerr@yandex.ru>
2017-09-04 17:42:32 +0300

    2

--- Experimental.py
+++ Experimental.py
@@ -1,3 +1,8 @@
-Year = input()
-if 1900 =< Year => 3000:
-    if Year % 4 and
+Year = int(input())
+if 1900 <= Year <= 3000:
+    if not((((Year % 4)==0) and not((Year % 100)==0)) or ((Year % 400)==0)):
+        print ('Високосный')
+    else:
+        print ('Обычный')
+else:
+    print ('Ошибка диапазона')
--- tmp.py
+++ tmp.py
@@ -1,2 +1,5 @@
-A = 20%4
-print (A)
+Year = int(input())
+if not(((Year % 4)==0) and not((Year % 100)==0)):
+    print ('True',Year%4,Year//100)
+else:
+    print ('False',Year%4,Year//100)
