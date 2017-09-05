b18abd9e298e34ea9f7bc8f1649db0a2b41a0e7d
airosa-id <registerr@yandex.ru>
2017-09-05 17:25:37 +0300

    Complete

--- Experimental.py
+++ Experimental.py
@@ -1,3 +1,27 @@
-Year = int(input())
-if 1900 <= Year <= 3000:
- print('Високосный' if not Year%400 or not Year%4 and Year%100 else 'Обычный')
+'''
+Напишите программу, принимающую на вход целое число, которая выводит True,
+если переданное значение попадает в интервал (−15,12]∪(14,17)∪[19,+∞) и False
+в противном случае (регистр символов имеет значение).
+
+'''
+a = float(input())
+b = float(input())
+operation = input('+,-,/,*,mod,pow,div')
+if operation == '+':
+    print (a+b)
+elif operation == '-':
+    print (a-b)
+elif operation == '/':
+    if b == 0:
+        print ('Деление на 0!')
+    else:
+        print (a/b)
+elif operation == '*':
+    print (a*b)
+elif operation == 'mod':
+    print (a//b)
+elif operation == 'pow':
+    print (a**b)
+elif operation == 'div':
+    print (a%b)
+
