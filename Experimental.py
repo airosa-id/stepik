c4570d4e1d383a0929cd40367a0a503b8ec1d2da
airosa-id <registerr@yandex.ru>
2017-09-05 17:28:33 +0300

    Complete

--- Experimental.py
+++ Experimental.py
@@ -1,9 +1,3 @@
-'''
-Напишите программу, принимающую на вход целое число, которая выводит True,
-если переданное значение попадает в интервал (−15,12]∪(14,17)∪[19,+∞) и False
-в противном случае (регистр символов имеет значение).
-
-'''
 a = float(input())
 b = float(input())
 operation = input('+,-,/,*,mod,pow,div')
@@ -19,9 +13,15 @@ elif operation == '/':
 elif operation == '*':
     print (a*b)
 elif operation == 'mod':
-    print (a//b)
+    if b == 0:
+        print ('Деление на 0!')
+    else:
+        print(a % b)
 elif operation == 'pow':
     print (a**b)
 elif operation == 'div':
-    print (a%b)
+    if b == 0:
+        print ('Деление на 0!')
+    else:
+        print(a // b)
 
