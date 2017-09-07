5e60fa15e7846f560251f6d6a4a0925fdb7860bc
airosa-id <registerr@yandex.ru>
2017-09-07 17:34:37 +0300

    Complete

--- Experimental.py
+++ Experimental.py
@@ -1,27 +1,14 @@
-a = float(input())
-b = float(input())
-operation = input('+,-,/,*,mod,pow,div')
-if operation == '+':
-    print (a+b)
-elif operation == '-':
-    print (a-b)
-elif operation == '/':
-    if b == 0:
-        print ('Деление на 0!')
-    else:
-        print (a/b)
-elif operation == '*':
-    print (a*b)
-elif operation == 'mod':
-    if b == 0:
-        print ('Деление на 0!')
-    else:
-        print(a % b)
-elif operation == 'pow':
-    print (a**b)
-elif operation == 'div':
-    if b == 0:
-        print ('Деление на 0!')
-    else:
-        print(a // b)
-
+type = input()
+if type == 'треугольник':
+    a = float(input())
+    b = float(input())
+    c = float(input())
+    p = (a+b+c)/2
+    print((p*(p - a)*(p - b)*(p - c))**0.5)
+elif type == 'прямоугольник':
+    a = float(input())
+    b = float(input())
+    print (a * b)
+elif type == 'круг':
+    r = float(input())
+    print (3.14 * (r**2))
