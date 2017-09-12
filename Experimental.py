6ee8fa533c8cf55751719fc10843e93dfda83e76
airosa-id <registerr@yandex.ru>
2017-09-12 17:42:39 +0300

    Complete

--- Experimental.py
+++ Experimental.py
@@ -1,14 +1,20 @@
-type = input()
-if type == 'треугольник':
-    a = float(input())
-    b = float(input())
-    c = float(input())
-    p = (a+b+c)/2
-    print((p*(p - a)*(p - b)*(p - c))**0.5)
-elif type == 'прямоугольник':
-    a = float(input())
-    b = float(input())
-    print (a * b)
-elif type == 'круг':
-    r = float(input())
-    print (3.14 * (r**2))
+programmers = int(input())
+swarm_progs = int()
+last = str()
+if 100 <= programmers < 999:
+    swarm_progs = programmers % 100
+    if swarm_progs > 20:
+        swarm_progs = programmers % 10
+
+if 20 < programmers < 100:
+    swarm_progs = programmers % 10
+
+if programmers == 1 or swarm_progs == 1:
+    last = ""
+elif (2 <= programmers <= 4) or (2 <= swarm_progs <= 4):
+    last = "а"
+elif (5 <= programmers <= 20) or (programmers == 0) or (programmers == 1000) or (5 <= swarm_progs <= 20) or (swarm_progs == 0):
+    last = "ов"
+
+print (swarm_progs)
+print (str(programmers) +" программист"+ str(last))
--- tmp.py
+++ tmp.py
@@ -1,6 +1,1 @@
-Year = int(input())
-if not(((Year % 4)==0) and not((Year % 100)==0)):
-    print ('True',Year%4,Year//100)
-else:
-    print ('False',Year%4,Year//100)
- dd
+print (100 % 10)
