1f27aa5170ac598e89bdbf1de30dceeccaa2b1ff
airosa-id <registerr@yandex.ru>
2017-09-13 16:03:22 +0300

    Complete

--- Experimental.py
+++ Experimental.py
@@ -1,20 +1,19 @@
-programmers = int(input())
-swarm_progs = int()
-last = str()
-if 100 <= programmers < 999:
-    swarm_progs = programmers % 100
-    if swarm_progs > 20:
-        swarm_progs = programmers % 10
-
-if 20 < programmers < 100:
-    swarm_progs = programmers % 10
-
-if programmers == 1 or swarm_progs == 1:
-    last = ""
-elif (2 <= programmers <= 4) or (2 <= swarm_progs <= 4):
-    last = "а"
-elif (5 <= programmers <= 20) or (programmers == 0) or (programmers == 1000) or (5 <= swarm_progs <= 20) or (swarm_progs == 0):
-    last = "ов"
-
-print (swarm_progs)
-print (str(programmers) +" программист"+ str(last))
+bilet = int(input())
+bil1,bil2,bil3,bil4,bil5,bil6, = int(),int(),int(),int(),int(),int()
+if bilet <= 999999:
+    bil6 = bilet % 10
+    next_num= bilet // 10
+    bil5 = next_num % 10
+    next_num= next_num // 10
+    bil4 = next_num % 10
+    next_num= next_num // 10
+    bil3 = next_num % 10
+    next_num= next_num // 10
+    bil2 = next_num % 10
+    next_num= next_num // 10
+    bil1 = next_num % 10
+    next_num= next_num // 10
+    if bil1+bil2+bil3 == bil4+bil5+bil6:
+        print('Счастливый')
+    else:
+        print('Обычный')
