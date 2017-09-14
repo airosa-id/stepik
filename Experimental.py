8e6513be4a3d733349fe1dc195a65162ef5f937d
airosa-id <registerr@yandex.ru>
2017-09-14 17:51:26 +0300

    Complete

--- Experimental.py
+++ Experimental.py
@@ -1,19 +1,11 @@
-bilet = int(input())
-bil1,bil2,bil3,bil4,bil5,bil6, = int(),int(),int(),int(),int(),int()
-if bilet <= 999999:
-    bil6 = bilet % 10
-    next_num= bilet // 10
-    bil5 = next_num % 10
-    next_num= next_num // 10
-    bil4 = next_num % 10
-    next_num= next_num // 10
-    bil3 = next_num % 10
-    next_num= next_num // 10
-    bil2 = next_num % 10
-    next_num= next_num // 10
-    bil1 = next_num % 10
-    next_num= next_num // 10
-    if bil1+bil2+bil3 == bil4+bil5+bil6:
-        print('Счастливый')
-    else:
-        print('Обычный')
+a = int(input())
+b = int(input())
+maxshare = int()
+i = int(1)
+
+while i <= 9:
+    if a%i == 0 and b%i == 0:
+        maxshare = i
+    i += 1
+
+print(a*b/maxshare)
